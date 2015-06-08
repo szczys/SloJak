29c7e3f778f5916cb847005aeee0041d302923be
Mike Szczys <szczys@hotmail.com>
2015-06-07 21:29:50 -0500

    Working display

--- main.c
+++ main.c
@@ -46,34 +46,33 @@ void oledWriteCmd(uint8_t cmd) {
     i2c_stop();
 }
 
-void oledSetCursor(uint8_t page, uint8_t col) {
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    //Page
-    i2c_write(0xB0 + page);
-    //Column
-    i2c_write(0x0F & col);
-    i2c_write(0x10 + (col>>4));
+void oledWriteData(uint8_t data) {
+    i2c_start_wait(OLED_ADDRESS+I2C_WRITE);
+    i2c_write(OLED_DATA_MODE);
+    i2c_write(data);
     i2c_stop();
+}
+
+void oledSetCursor(uint8_t page, uint8_t col) {
+    //Page
+    oledWriteCmd(0xB0 + page);
+    //Column
+    oledWriteCmd((0x0F & col)+2);
+    oledWriteCmd(0x10 + (col>>4));
 }
 
 void oledClearScreen(uint8_t black) {
     uint8_t value = 0xFF;
     if (black) { value = 0x00; }
     for (uint8_t page=0; page<8; page++) {
-        i2c_start_wait(OLED_ADDRESS);
-        i2c_write(OLED_COMMAND_MODE);
-        i2c_write(0xB0 + page);
-        i2c_write(0x00);
-        i2c_write(0x10);
+        oledWriteCmd(0xB0 + page);
+        oledWriteCmd(0x00);
+        oledWriteCmd(0x10);
         i2c_stop();
-        
-        i2c_start_wait(OLED_ADDRESS);
-        i2c_write(OLED_DATA_MODE);
+
         for (uint8_t col=0; col<128; col++) {
-            i2c_write(value);
+            oledWriteData(value);
         }
-        i2c_stop();
     }
 }
 
@@ -83,37 +82,44 @@ int main(void)
     _delay_ms(200);
     i2c_init();
 
-    _delay_ms(10);
-    i2c_start_wait(OLED_ADDRESS+I2C_WRITE);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(OLED_CMD_DISPLAY_OFF);
-    i2c_stop();
-    _delay_ms(10);
+    oledWriteCmd(OLED_CMD_DISPLAY_OFF);
+    oledWriteCmd(0xD5); //clkdiv
+    oledWriteCmd(0x80); 
+    oledWriteCmd(0xA8); //multiplex
+    oledWriteCmd(0x3F);
+    oledWriteCmd(0xD3); //displayoffet
+    oledWriteCmd(0x00);
+    oledWriteCmd(0x40); //displayStartLine
+    oledWriteCmd(0x8D);
+    oledWriteCmd(0x14);
+    oledWriteCmd(0x20);
+    oledWriteCmd(PAGE_ADDRESSING);
+    oledWriteCmd(0xA1); //segremap 0x01
+    oledWriteCmd(0xC8); //comscandec
+    oledWriteCmd(0xDA);
+    oledWriteCmd(0x12);
+    oledWriteCmd(0x81);
+    oledWriteCmd(0xCF);
+    oledWriteCmd(0xD9);
+    oledWriteCmd(0xF1);
+    oledWriteCmd(0xDB);
+    oledWriteCmd(0x40);
 
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(OLED_CMD_DISPLAY_ON);
-    i2c_stop();
-    _delay_ms(10);
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(OLED_CMD_NORMAL_DISPLAY);
-    i2c_stop();
-    /*
-    _delay_ms(10);
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(OLED_CMD_NORMAL_DISPLAY);
-    i2c_stop();
-    */
-    _delay_ms(10);
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(0x20);
-    i2c_write(PAGE_ADDRESSING);
-    i2c_stop();
-    _delay_ms(10);
-    
+    oledWriteCmd(0x26);
+
+    oledWriteCmd(0x00);
+    oledWriteCmd(0x05);
+    oledWriteCmd(0x00);
+    oledWriteCmd(0x07);
+
+    oledWriteCmd(0x2F);
+    //oledWriteCmd(0x2E);
+
+    //oledWriteCmd(0x26);
+    oledWriteCmd(OLED_CMD_DISPLAY_ON);
+    oledWriteCmd(OLED_CMD_NORMAL_DISPLAY);
+
+/*
     i2c_start_wait(OLED_ADDRESS);
     i2c_write(OLED_COMMAND_MODE);
     i2c_write(0x26);
@@ -124,56 +130,16 @@ int main(void)
     i2c_write(0x00);
     i2c_write(0xFF);
     i2c_stop();
-
+*/
     oledClearScreen(1);
 
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(0xB2);
-    i2c_stop();
-
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_DATA_MODE);
-    for (uint8_t i=0; i<32; i++) {
-    i2c_write(0x00);
-    i2c_write(0xFF);
+    for (uint8_t i=0; i<8; i++) {
+        oledSetCursor(i,0);
+        oledWriteData(0xFF);
+        oledSetCursor(i,127);
+        oledWriteData(0xFF);
     }
-    i2c_stop();
 
-    //oledSetCursor(4,20);
-
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(0xB4);
-    i2c_write(0x00);
-    i2c_write(0x16);
-    i2c_stop();
-
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_DATA_MODE);
-    for (uint8_t i=0; i<12; i++) {
-    i2c_write(0x00);
-    i2c_write(0xFF);
-    }
-    i2c_stop();
-    
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(0xB5);
-    i2c_write(0x01);
-    i2c_write(0x10);
-    i2c_stop();
-
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_DATA_MODE);
-    i2c_write(0xFF);
-    i2c_stop();
-
-    i2c_start_wait(OLED_ADDRESS);
-    i2c_write(OLED_COMMAND_MODE);
-    i2c_write(0x26);
-    i2c_write(0x2F);
-    i2c_stop();
   while(1)
   {
     //wait for a little bit before repeating everything
